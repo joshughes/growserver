7460d203c7ba06b692a24e23fe61d9182f35baee
Joseph Hughes <jhughes@itriagehealth.com>
2014-09-21 15:14:21 -0600

    remove debugging

--- devices.js
+++ devices.js
@@ -35,7 +35,7 @@ var readAnalogDevice = function(error, device, call_back) {
   } else if(error) {
     call_back(error, device)
   } else {
-    error = "Device is Digital not Analog, can not take reading"
+    error.errors = { type: { message: "Wrong Device Type"} }
     call_back(error, device)
   }
 }
